de88d8aa53bc0d31adabeb58eabdf51b9048673b
boablock <boablockchain@gmail.com>
2022-12-01 21:22:27 -0300

    PreEntrega1-Boada

--- src/Components/CartWidget/CartWidget.js
+++ src/Components/CartWidget/CartWidget.js
@@ -6,8 +6,14 @@ import React from 'react'
 import './CartWidget.css'
 
 const CartWidget = () => {
+    
   return (
-    <i className=" fa-2x cart bi bi-cart" ></i>
+    <div>
+      <a href="...">
+        <i className="bi bi-cart3 h2 link-danger cart-widget position-relative"></i>
+      </a>
+      <span className=" cart-counter position-absolute bg-white text-center text-danger translate-middle h5 ">10</span>
+    </div>
     
   )
 }
