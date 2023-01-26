1d0a04034f1ee54cd5afb0a422a096f4daeeb6bd
boablock <boablockchain@gmail.com>
2023-01-26 16:42:30 -0300

    first commit

--- src/Components/Nabvar/Navbar.js
+++ src/Components/Nabvar/Navbar.js
@@ -6,66 +6,31 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className=" nav-bar navbar1navbar-boxes navbar navbar-expand-lg bg-light">
-      <div className=" nav-bar container-fluid">
-        <NavLink className="navbar-brand" to="./">
-          <img className="logo" src=".\Assets\logo.png" alt="" />
-        </NavLink>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div
-          className=" d-flex justify-content-center navbar-boxes collapse navbar-collapse"
-          id="navbarNav"
-        >
-          <ul
-            className=" d-flex justify-content-around navbar-boxes navbar-nav"
-            style={{ width: "70%" }}
-          >
-            <li className="nav-item">
-              <NavLink
-                className="nav-link active"
-                aria-current="page"
-                to="./tienda"
-              >
-                Tienda
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="./suscripciones">
-                Suscripciones
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="./nosotras">
-                Nosotras
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="#">
-                Eventos
-              </NavLink>
-            </li>
-            <li className=" nav-item-cart">
-              <NavLink className=" nav-link " to="./cart">
-                <CartWidget />
-              </NavLink>
-            </li>
-          </ul>
-          {/* <NavLink className="cart nav-link " to="./cart">
-                        <CartWidget/>
-                      </NavLink> */}
-        </div>
-      </div>
-    </nav>
+    <>
+      <nav className="menu">
+        <label className="logo"><img src="../../../public/Assets/logo.png"/></label>
+        <ul className="menu_items">
+          <li>
+            <NavLink to="#">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="#">Tienda</NavLink>
+          </li>
+          <li>
+            <NavLink to="#">Nosotras</NavLink>
+          </li>
+          <li>
+            <NavLink to="#">Eventos</NavLink>
+          </li>
+          <li>
+            <NavLink to="#">Contacto</NavLink>
+          </li>
+        </ul>
+        <span className="btn-menu">
+          <i class="fa-solid fa-bars"></i>
+        </span>
+      </nav>
+    </>
   );
 };
 
